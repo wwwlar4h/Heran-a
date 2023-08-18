03b0721f04550f1119efa246b9612376ad0d5b49
Larah <schulteslarah@gmail.com>
2023-08-18 14:37:19 -0400

    Atividade herança

---
+++ Atividade Herança/ContaEmpres.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade_Herança
+{
+    internal class ContaEmpres
+    {
+    }
+}
