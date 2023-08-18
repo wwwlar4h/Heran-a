e4b30ba7a21b5f6378e29ef3762df06138cb29b4
Larah <schulteslarah@gmail.com>
2023-08-18 14:46:29 -0400

    gh

--- Atividade Herança/ContaEmpres.cs
+++ Atividade Herança/ContaEmpres.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Security.Cryptography;
 
 namespace Atividade_Herança
 {
-    internal class ContaEmpres
+    internal class ContaEmpres : Conta
     {
+        public double anuidade { get; set; }
+        public double limiteEmp { get; set; }
+        public double totalEmp { get; set; }
+
+
+        public ContaEmpres(int numCon, int agencia, string titularCon, double saldo, double anuidade, double limiteEmp):base(numCon,agencia,titularCon, saldo)
+        {
+            this.anuidade = anuidade;
+            this.limiteEmp = limiteEmp;
+            this.totalEmp = totalEmp;
+        }
+        public void Emprestimo(double valor)
+        {
+
+        }
+
+        public void Sacar(double valorr)
+        {
+
+        }
     }
 }
